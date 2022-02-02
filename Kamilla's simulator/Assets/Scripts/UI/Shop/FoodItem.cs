ad7f74afa740081566d807d9ca44720b7fe89a17
TiimmyTurner <shushlyakovd@mail.ru>
2022-02-02 18:17:49 +0300

    02.02.22

--- Kamilla's simulator/Assets/Scripts/UI/Shop/FoodItem.cs
+++ Kamilla's simulator/Assets/Scripts/UI/Shop/FoodItem.cs
@@ -7,8 +7,10 @@ public class FoodItem : MonoBehaviour
     [SerializeField] private string _label;
     [SerializeField] private int _price;
     [SerializeField] private Sprite _icon;
+    [SerializeField] private int _energyValue;
 
     public string Label => _label;
     public int Price => _price;
     public Sprite Icon => _icon;
+    public int EnergyValue => _energyValue;
 }
